6a389d6ade6178fa10b435e255bf57e783276af8
Sasha <ivanov_sasha2000@mail.ru>
2023-05-18 13:38:44 +0300

    added

---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <img src="../assets/gallery_img_1.jpg">
+    <img src="../assets/gallery_img_2.jpg">
+    <img src="../assets/gallery_img_3.jpg">
+    <img src="../assets/gallery_img_4.jpg">
+  </div>
+</template>
+
+<style scoped>
+div {
+  /*height: 500px;
+  width: 750px;
+  overflow: scroll;*/
+
+  width: 98%;
+  height: 500px;
+  margin: auto;
+  
+  display: flex;
+  flex-direction: row;
+  overflow: scroll;
+}
+img {
+  height: 100%;
+}
+</style>
---
+++ src/components/UpperMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img src="../assets/vdnh_side_logo.png" class="logo">
+    <span class="menuItem">События</span>
+    <span class="menuItem">Посетителям</span>
+    <span class="menuItem">Билеты</span>
+    <span class="menuItem">Скидки</span>
+    <span class="menuItem">Новости</span>
+    <span class="menuItem">Контакты</span>
+    <span class="menuItem">Ещё...</span>
+  </div>
+</template>
+
+<style scoped>
+div {
+  border: 1px solid black;
+  border-radius: 5px;
+  height: 75px;
+  width: 98%;
+  margin: auto;
+
+  display: flex;
+  align-items: center;
+}
+.logo {
+  height: 60px;
+}
+.menuItem {
+  padding-left: 30px;
+}
+</style>
